3b186bf2ea501eaed85ce6d2bb700e38c51d3bcd
Sarina Reuschle <sarina213@Sarinas-MacBook-Air.local>
2017-10-12 10:41:39 -0700

    Added blog stuff

--- blog_crawler.rb
+++ blog_crawler.rb
@@ -1,5 +1,5 @@
 class Crawler
-	def say_hello(thing_to_say) 
+	def say_hello(thing_to_say)
 		puts thing_to_say
 	end
 end
--- run.rb
+++ run.rb
@@ -2,3 +2,4 @@ require './blog_crawler.rb'
 
 crawler = Crawler.new
 crawler.say_hello(ARGV[0])
+

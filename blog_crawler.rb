91ff323fdd818054f13d1ed4e55d6da4825ed0a5
Sarina Reuschle <sarina213@Sarinas-Air.local.tld>
2017-10-18 17:34:55 -0700

    added html site

--- blog_crawler.rb
+++ blog_crawler.rb
@@ -1,8 +1,57 @@
+#class Crawler
+#	def say_hello(thing_to_say)
+#		puts thing_to_say
+#end
+
+class BlogEntry
+	attr_reader :date
+	attr_accessor :author, :title, :tags, :shares
+end
+
+#class Crawler
+#	def crawl
+#		i = 0
+
+#		while i < 3  do			
+#			puts i
+#			i = i + 1
+#		end
+#	end
+#end
+
+#class Crawler
+#	def crawl
+#		i = 3
+
+#		begin
+#			puts i
+#			i = i + 1
+#		end while i < 3
+#	end
+#end
+
+#class Crawler
+#	def crawl
+#		blog_names = ['Blog 1', 'Blog 2', 'Blog 3']
+#
+### 	end
+	#end
+#end
+
 class Crawler
-	def say_hello(thing_to_say)
-		puts thing_to_say
+	def crawl
+		entry = BlogEntry.new
+		entry.author = "Sarina"
+		entry.title = "Paper Paper Paper"
+
+		display_name = "#{entry.title}, By: #{entry.author}"
+		puts display_name
 	end
 end
+
+
+
+
 
 
 
--- run.rb
+++ run.rb
@@ -1,5 +1,16 @@
+#require './blog_crawler.rb'
+
+#crawler = Crawler.new
+#crawler.say_hello(ARGV[0])
+
 require './blog_crawler.rb'
 
+new_blog = BlogEntry.new
+new_blog.author = "Sarina"
+
+puts new_blog.author
+
 crawler = Crawler.new
-crawler.say_hello(ARGV[0])
+crawler.crawl
+
 
